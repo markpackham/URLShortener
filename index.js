4f0fda8b9a92ebeecadd51c720d47b10a28fa051
Mark Packham <markpackham1@gmail.com>
2019-07-10 18:52:08 +0100

    make sure we're connected to the database https://youtu.be/Z57566JBaZQ?t=663

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
+// Connect to database
+connectDB();
+
 app.use(express.json({ extended: false }));
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
