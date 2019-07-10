64075441ab0d5daa7c7ba68e99782fec73d63b01
Mark Packham <markpackham1@gmail.com>
2019-07-10 19:39:16 +0100

    url validation https://youtu.be/Z57566JBaZQ?t=1059

--- routes/url.js
+++ routes/url.js
@@ -6,4 +6,15 @@ const config = require("config");
 
 const Url = require("../models/URL");
 
+// @route POST /api/url/shorten
+// @desc Create short URL
+router.post("/shorten", (req, res) => {
+  const { longUrl } = req.body;
+  const baseUrl = config.get("baseUrl");
+
+  if(){
+      
+  }
+});
+
 module.exports = router;
