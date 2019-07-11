f910786dfcf2b395a2f3d4c967fd331a2a1e7c9e
Mark Packham <markpackham1@gmail.com>
2019-07-11 09:32:26 +0100

    correct reuired model name

--- routes/url.js
+++ routes/url.js
@@ -4,10 +4,10 @@ const validUrl = require("valid-url");
 const shortid = require("shortid");
 const config = require("config");
 
-const Url = require("../models/URL");
+const Url = require("../models/Url");
 
-// @route POST /api/url/shorten
-// @desc Create short URL
+// @route     POST /api/url/shorten
+// @desc      Create short URL
 router.post("/shorten", async (req, res) => {
   const { longUrl } = req.body;
   const baseUrl = config.get("baseUrl");
@@ -16,6 +16,7 @@ router.post("/shorten", async (req, res) => {
   if (!validUrl.isUri(baseUrl)) {
     return res.status(401).json("Invalid base url");
   }
+
   // Create url code
   const urlCode = shortid.generate();
 
@@ -23,16 +24,19 @@ router.post("/shorten", async (req, res) => {
   if (validUrl.isUri(longUrl)) {
     try {
       let url = await Url.findOne({ longUrl });
+
       if (url) {
         res.json(url);
       } else {
         const shortUrl = baseUrl + "/" + urlCode;
+
         url = new Url({
           longUrl,
           shortUrl,
           urlCode,
           date: new Date()
         });
+
         await url.save();
 
         res.json(url);
