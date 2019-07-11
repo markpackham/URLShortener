f7a7f610f77a1bba5fb59499ef5a0080406e4351
Mark Packham <markpackham1@gmail.com>
2019-07-11 09:29:39 +0100

    work on requests and url routes

--- routes/url.js
+++ routes/url.js
@@ -8,12 +8,41 @@ const Url = require("../models/URL");
 
 // @route POST /api/url/shorten
 // @desc Create short URL
-router.post("/shorten", (req, res) => {
+router.post("/shorten", async (req, res) => {
   const { longUrl } = req.body;
   const baseUrl = config.get("baseUrl");
 
-  if(){
-      
+  // Check base url
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(401).json("Invalid base url");
+  }
+  // Create url code
+  const urlCode = shortid.generate();
+
+  // Check long url
+  if (validUrl.isUri(longUrl)) {
+    try {
+      let url = await Url.findOne({ longUrl });
+      if (url) {
+        res.json(url);
+      } else {
+        const shortUrl = baseUrl + "/" + urlCode;
+        url = new Url({
+          longUrl,
+          shortUrl,
+          urlCode,
+          date: new Date()
+        });
+        await url.save();
+
+        res.json(url);
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json("Server error");
+    }
+  } else {
+    res.status(401).json("Invalid long url");
   }
 });
 
